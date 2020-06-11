671a49f674f3a657ec28aa88593ccafd3de46b3d
Alex Ngundji <alexngundji@gmail.com>
2020-06-11 15:19:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,20 +1,25 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
+  ["alex","chris","boris","sonia"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+   array << element
   
 end
 
 def add_element_to_start_of_array(array, element)
+  array.unshift("wow")
   
 end
 
 def remove_element_from_end_of_array(array)
+  array.pop
   
 end
 
